3b5552422dc77ad170cb83f61b0f5f8cb5c5cef3
JoaoBringmann <149213160+JoaoBringmann@users.noreply.github.com>
2024-04-16 16:17:20 -0300

    Inicio

---
+++ Pokemon.py
@@ -0,0 +1,7 @@
+import random
+
+nome_jogador = input("Qual é o seu nome aventureiro? ")
+print(f"Olá {nome_jogador}, prepare-se para uma emocionante jornada!")
+
+pokemons_caverna = ["Zubat", "Grimer", "Cubone", "Geodude"]
+pokemons_mato = ["Pidgey", "Rattata", "Ekans", "Oddish"]
