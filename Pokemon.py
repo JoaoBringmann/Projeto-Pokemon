19a3074c7dbeee78f6a39de836ce76cc27d7d861
JoaoBringmann <149213160+JoaoBringmann@users.noreply.github.com>
2024-04-20 13:00:34 -0300

    Final

--- Pokemon.py
+++ Pokemon.py
@@ -24,3 +24,67 @@ def pokemon_mato():
     else:
         print("Nenhum Pokémon apareceu nesta exploração.")
         return None
+    
+def informar_pokemon_encontrado(pokemon_encontrado):
+    print(f"\nParabéns, {nome_jogador}! Você encontrou um {pokemon_encontrado}!")
+    captura = input((f"Deseja captura-lo? (sim/não)"))
+    if captura == "sim":
+        if random.random() < chance_captura:
+            capturar_pokemon(pokemon_encontrado)
+            print(f"Parabéns, {nome_jogador}! Você capturou um {pokemon_encontrado}!")
+        else:
+            print(f"Infelizmente, o {pokemon_encontrado} fugiu!")
+
+def capturar_pokemon(pokemon_encontrado):
+    global pokemon_capturados
+
+    if pokemon_encontrado in pokemon_capturados:
+        print(f"O {pokemon_encontrado} já está na sua Pokédex, {nome_jogador}!")
+    else:
+        pokemon_capturados.append(pokemon_encontrado)
+        print(f"Parabéns, {nome_jogador}! Você capturou um {pokemon_encontrado}!")
+
+def encontrou_pokemon():
+    if len(pokemon_capturados) < 3:
+        return True
+    else:
+        resposta = input("Você já encontrou três Pokémons neste ambiente. Deseja procurar em outro ambiente? (sim/não) ").lower()
+        if resposta == "sim":
+            ambiente = input("Em que ambiente deseja explorar? Digite 'caverna' ou 'mato': ").lower()
+            if ambiente == "caverna":
+                pokemon_encontrado = pokemon_caverna()
+            elif ambiente == "mato":
+                pokemon_encontrado = pokemon_mato()
+            else:
+                print("Digite Novamente uma das Opções.")
+                ambiente = input("Em que ambiente deseja explorar? Digite 'caverna' ou 'mato': ").lower()
+                pokemon_encontrado = None if ambiente == "caverna" else pokemon_mato()
+            return encontrou_pokemon()
+        else:
+            print("Tudo bem, volte quando quiser tentar novamente!")
+            return False
+
+while True:
+    menu = input("O que deseja fazer?\nExplorar\nPokedex\nSair\n").lower()
+    if menu == "explorar":
+        ambiente = input("Em que ambiente deseja explorar? Digite 'caverna' ou 'mato': ").lower()
+        if ambiente == "caverna":
+            pokemon_encontrado = pokemon_caverna()
+        elif ambiente == "mato":
+            pokemon_encontrado = pokemon_mato()
+        else:
+            print("Desculpe, não entendi. Por favor, digite 'caverna' ou 'mato'.")
+            ambiente = input("Em que ambiente deseja explorar? Digite 'caverna' ou 'mato': ").lower()
+            pokemon_encontrado = None if ambiente == "caverna" else pokemon_mato()
+
+        if pokemon_encontrado:
+            informar_pokemon_encontrado(pokemon_encontrado)
+    elif menu == "pokedex":
+        print("\nSeus Pokémons capturados são:")
+        for pokemon in pokemon_capturados:
+            print(f"- {pokemon}")
+    elif menu == "sair":
+        print("Até logo, aventureiro!")
+        break
+    else:
+        print("Digite novamente")
