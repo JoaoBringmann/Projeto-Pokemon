f8d37f8e172d6a0a4ffcbbedcd2d7c59221de47b
JoaoBringmann <149213160+JoaoBringmann@users.noreply.github.com>
2024-05-10 21:32:41 -0300

    Parte 2
    Foi adicionada as funções relacionada as pokebolas, assim como novas mensagens para o usuario

--- Pokemon.py
+++ Pokemon.py
@@ -1,25 +1,22 @@
-
 import random
 
 chance_caverna = 0.5
 chance_mato = 0.5
 chance_captura = 0.5
-
+chance_pokebolas = 0.3
 
 pokemons_iniciais = ["Charmander", "Squirtle", "Bulbasaur"]
 pokemons_caverna = ["Zubat", "Grimer", "Cubone", "Geodude"]
 pokemons_mato = ["Pidgey", "Rattata", "Ekans", "Oddish"]
 
-
 nome_jogador = input("Qual é o seu nome aventureiro? ")
 print(f"Olá {nome_jogador}, prepare-se para uma emocionante jornada!")
 
+pokebolas = 3
 
 pokemon_capturados = []
 
 primeiro_pokemon = []
-
-#-----Funções-----#
 
 def pokemon_inicial(primeiro_pokemon):
     pokemon_inicial = input("Escolha um dos seguintes pokemons para começar a sua jornada: Charmander, Squirtle, Bulbasaur\n").lower()
@@ -29,7 +26,6 @@ def pokemon_inicial(primeiro_pokemon):
     primeiro_pokemon.append(pokemon_inicial)
     pokemon_capturados.append(pokemon_inicial)
     return print(f"Seu pokemon inicial foi confirmado como: -{primeiro_pokemon[0]}-")
-
 
 def sorteio_pokemon(lista_pokemon):
     return random.choice(lista_pokemon)
@@ -52,6 +48,7 @@ def pokemon_mato():
         return None
 
 def informar_pokemon_encontrado(pokemon_encontrado):
+    global pokebolas
     print(f"\nParabéns, {nome_jogador}! Você encontrou um {pokemon_encontrado}!")
     captura = input(f"Deseja captura-lo? (sim/não) ").lower()
     while captura not in ["sim", "não"]:
@@ -62,18 +59,29 @@ def informar_pokemon_encontrado(pokemon_encontrado):
         if pokemon_encontrado in pokemon_capturados:
             print("Você já possui este Pokemon")
         else:
+            pokebolas -= 1
             print(80*"-")
             print(f"Parabéns, {nome_jogador}! Você capturou um {pokemon_encontrado}!")
             print(80*"-")
             pokemon_capturados.append(pokemon_encontrado)
     elif captura == "não":
-        if capturar_pokemon(pokemon_encontrado, chance_captura):
-            print(f"Parabéns, {nome_jogador}! Você capturou um {pokemon_encontrado}!")
-            pokemon_capturados.append(pokemon_encontrado)
-        else:
-            print(f"Infelizmente, o {pokemon_encontrado} fugiu!")
-        
+        print("Ok, voltando para o menu...")
+    else:
+        print(f"Infelizmente, o {pokemon_encontrado} fugiu!")
+
+def adquirir_pokebolas():
+    global pokebolas
+    print(f"Você possui:{pokebolas}, pokebolas")
+    if random.random() < chance_pokebolas:
+        qtd_pokebolas = random.randint(1, 2)
+        pokebolas += qtd_pokebolas
+        print(f"Parabéns, {nome_jogador}! Você adquiriu {qtd_pokebolas} pokebolas!")
+    else:
+        print("Nenhuma pokebola adquirida desta vez")
+
+
 def menu():
+    global pokebolas
     pokemon_inicial(primeiro_pokemon)
     while True:
         menu = input("O que deseja fazer?\nExplorar\nPokedex\nSair\n").lower()
@@ -82,17 +90,36 @@ def menu():
             menu = input("O que deseja fazer?\nExplorar\nPokedex\nSair\n").lower()
 
         if menu == "explorar":
-            ambiente = input("Em que ambiente deseja explorar? Digite 'caverna' ou 'mato': ").lower()
-            if ambiente == "caverna":
-                pokemon_encontrado = pokemon_caverna()
-                if pokemon_encontrado:
-                    informar_pokemon_encontrado(pokemon_encontrado)
-            elif ambiente == "mato":
-                pokemon_encontrado = pokemon_mato()
-                if pokemon_encontrado:
-                    informar_pokemon_encontrado(pokemon_encontrado)
+            if pokebolas > 0:
+                ambiente = input("Em que ambiente deseja explorar? Digite 'caverna' ou 'mato': ").lower()
+                if ambiente == "caverna":
+                    pokemon_encontrado = pokemon_caverna()
+                    if pokemon_encontrado:
+                        adquirir_pokebolas()
+                        informar_pokemon_encontrado(pokemon_encontrado)
+                        if pokemon_encontrado not in pokemon_capturados:
+                         print(f"Nenhuma pokebola gasta")
+                         print(80*"-")
+                        else:
+                         print(f"Você gastou 1 pokebola.")
+                         print(80*"-")
+                        pokebolas -= 1
+                elif ambiente == "mato":
+                    pokemon_encontrado = pokemon_mato()
+                    if pokemon_encontrado:
+                        adquirir_pokebolas()
+                        informar_pokemon_encontrado(pokemon_encontrado)
+                        if pokemon_encontrado not in pokemon_capturados:
+                         print(f"Nenhuma pokebola gasta")
+                         print(80*"-")
+                        else:
+                         print(f"Você gastou 1 pokebola.")
+                         print(80*"-")
+                        pokebolas -= 1
+                else:
+                    print("Desculpe, não entendi. Por favor, digite 'caverna' ou 'mato'.")
             else:
-                print("Desculpe, não entendi. Por favor, digite 'caverna' ou 'mato'.")
+                print("Você não tem mais pokebolas. Não é mais possivel Explorar")
         elif menu == "pokedex":
             print("\nSeus Pokémons capturados são:")
             for pokemon in pokemon_capturados:
@@ -102,6 +129,5 @@ def menu():
         elif menu == "sair":
             print("\nObrigado por explorar este mundo Pokémon! Até a próxima!\n")
             break
-#-----Fim_Funções-----#
 
 menu()
